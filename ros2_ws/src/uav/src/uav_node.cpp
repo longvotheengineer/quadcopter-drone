2d564aab5dcf1ba30d5d80e05d38d5cdc95a87c5
longvotheengineer <longdaicangu0002@gmail.com>
2026-06-20 16:09:27 +0700

    create the publisher (#7)

--- ros2_ws/src/uav/src/uav_node.cpp
+++ ros2_ws/src/uav/src/uav_node.cpp
@@ -1,13 +1,18 @@
-#include "rclcpp/rclcpp.hpp"
 #include <chrono>
 #include <functional>
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/float64.hpp"
 
 using namespace std::chrono_literals;
 
 class UavNode : public rclcpp::Node {
 public:
-    UavNode() : Node("my_first_uav_node") {
+    UavNode() : Node("uav_node") {
         RCLCPP_INFO(this->get_logger(), "The node has started.");
+        
+        publisher_ = this->create_publisher<std_msgs::msg::Float64>(
+            "uav_altitude", 10
+        );
         timer_ = this->create_wall_timer(
             500ms, std::bind(&UavNode::timer_callback, this)
         );
@@ -15,15 +20,19 @@ public:
 
 private:
     void timer_callback() {
-        RCLCPP_INFO(this->get_logger(), "Test timer callback.");
+        auto message = std_msgs::msg::Float64();
+        message.data = 10.0;
+
+        RCLCPP_INFO(this->get_logger(), "[timer] triggered.");
+        publisher_->publish(message);
     }
 
     rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr publisher_;
 };
 
-
 int main(int argc, char **argv) {
-    rclcpp::init(argc, argv); // Initialize ROS 2
+    rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<UavNode>()); 
     rclcpp::shutdown(); 
     return 0;
