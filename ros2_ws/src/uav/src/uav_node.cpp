e4f1bae15f14aa2ff57b3fd3e55cc84fa4fd642a
longvotheengineer <longdaicangu0002@gmail.com>
2026-06-20 12:13:05 +0700

    finished testing timer and callback (Closes #5)

--- ros2_ws/src/uav/src/uav_node.cpp
+++ ros2_ws/src/uav/src/uav_node.cpp
@@ -1,11 +1,26 @@
-#include "rclcpp/rclcpp.hpp" 
+#include "rclcpp/rclcpp.hpp"
+#include <chrono>
+#include <functional>
+
+using namespace std::chrono_literals;
 
 class UavNode : public rclcpp::Node {
 public:
     UavNode() : Node("my_first_uav_node") {
         RCLCPP_INFO(this->get_logger(), "The node has started.");
+        timer_ = this->create_wall_timer(
+            500ms, std::bind(&UavNode::timer_callback, this)
+        );
     }
+
+private:
+    void timer_callback() {
+        RCLCPP_INFO(this->get_logger(), "Test timer callback.");
+    }
+
+    rclcpp::TimerBase::SharedPtr timer_;
 };
+
 
 int main(int argc, char **argv) {
     rclcpp::init(argc, argv); // Initialize ROS 2
